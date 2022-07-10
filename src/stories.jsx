02dc3e4f5c522b14589bb1dd42eaa892c820bc43
pacific23 <48202212+pacific23@users.noreply.github.com>
2022-07-11 00:07:11 +0200

    Update

--- src/stories.jsx
+++ src/stories.jsx
@@ -7,8 +7,8 @@ class Stories {
       id: 1,
       questions: ["Question 1", "Question 2", "Question 3", "Question 4"],
       plaquettes: [
-        [true, true, true, true, true, true],
-        [false, true, true, true, true, true]
+        [true, true, true, true, false, false],
+        [false, true, true, true, false, false]
       ],
       startInfos: [
         [0, 0, 0],
